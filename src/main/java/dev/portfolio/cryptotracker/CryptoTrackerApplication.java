745508b1118def0ea9af567e886ae52d80a3cb97
Emerson <griloemer@gmail.com>
2026-01-09 20:53:40 -0300

    Jpa Configs

--- src/main/java/dev/portfolio/cryptotracker/CryptoTrackerApplication.java
+++ src/main/java/dev/portfolio/cryptotracker/CryptoTrackerApplication.java
@@ -2,9 +2,10 @@ package dev.portfolio.cryptotracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "dev.portfolio.cryptotracker")
-//@EntityScan(basePackages = "dev.portfolio.cryptotracker.model")  // Ensure entities are scanned
+@EntityScan(basePackages = "dev.portfolio.cryptotracker.model")  // Ensure entities are scanned
 public class CryptoTrackerApplication {
 
 	public static void main(String[] args) {
