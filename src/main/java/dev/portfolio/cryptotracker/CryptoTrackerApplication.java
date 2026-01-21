23215701692c715c3cc318dc993ef1abf1d1647d
Emerson <griloemer@gmail.com>
2026-01-21 19:49:43 -0300

    Rodando local corretamente

--- src/main/java/dev/portfolio/cryptotracker/CryptoTrackerApplication.java
+++ src/main/java/dev/portfolio/cryptotracker/CryptoTrackerApplication.java
@@ -3,9 +3,11 @@ package dev.portfolio.cryptotracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "dev.portfolio.cryptotracker")
-@EntityScan(basePackages = "dev.portfolio.cryptotracker.model")  // Ensure entities are scanned
+@EntityScan("dev.portfolio.cryptotracker.domain.model")
+@EnableJpaRepositories("dev.portfolio.cryptotracker.port.out")
 public class CryptoTrackerApplication {
 
 	public static void main(String[] args) {
